055fdf263a5581e5420da18fb1f7cb29d37d7eee
NikiSv <svetanim@gmail.com>
2023-11-26 02:23:12 +0300

    настройки доступа для просмотра страницы пользователей для анонима пермишн v9

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -9,5 +9,7 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return (request.user.is_authenticated
-                and request.user == obj.author)
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_superuser
+                or request.user.is_staff
+                or obj.author == request.user)
