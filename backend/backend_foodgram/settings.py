f3cf69d98a7e5b3cd224e2b861b14d1785605159
NikiSv <svetanim@gmail.com>
2023-11-26 00:59:47 +0300

    настройки доступа для просмотра страницы пользователей для анонима v4

--- backend/backend_foodgram/settings.py
+++ backend/backend_foodgram/settings.py
@@ -30,9 +30,9 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
+    'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-    ),
+    ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
     ],
